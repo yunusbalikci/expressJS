8fd25566fef7b8914ac69998f0773bfa2069cf63
yunusbalikci <balikci.yunus06@gmail.com>
2024-10-13 20:05:33 +0300

    routing

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const config = require("../config");
+const fs = require("fs");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Yunus', config });
 });
+let routes = fs.readdirSync(__dirname);
+for (let route of routes) {
+  if (route.includes(".js") && route != "index.js") {
+    router.use("/"+route.replace(".js", ""), require('./'+route));
+  }
+}
 
 module.exports = router;
